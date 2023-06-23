8b9342cc373eb6a0789e0235c6030208d38268f2
Siarhei Sych <siarhei.sych@ihsmarkit.com>
2023-06-23 12:32:14 +0400

    add methods

--- common/element-wrapper/lib/elements.ts
+++ common/element-wrapper/lib/elements.ts
@@ -15,52 +15,66 @@ export class ElementHelper {
     });
   }
 
-  static async getElement(elLocator: string) {
-    return await BrowserHelper.page.$x(elLocator);
+  static async getElement(selector: string) {
+    return await BrowserHelper.page.$x(selector);
   }
 
-  public static async getAttributeValue(locator: string, properties: string) {
-    return await BrowserHelper.page.$eval(locator, element => element.getAttribute(properties));
+  public static async getAttributeValue(selector: string, properties: string) {
+    return await BrowserHelper.page.$eval(selector, element => element.getAttribute(properties));
   }
 
-  public static async scrollToElement(locator) {
-    const [span] = await ElementHelper.getElement(locator);
+  public static async scrollToElement(selector) {
+    const [span] = await ElementHelper.getElement(selector);
     return await BrowserHelper.page.evaluate(element => {
       element.scrollIntoView();
     }, span);
   }
 
-  static async getElementArray(elLocator: string) {
-    return await BrowserHelper.page.$$(elLocator);
+  static async getElementArray(selector: string) {
+    return await BrowserHelper.page.$$(selector);
   }
 
-  static async clickOnElement(elLocator: string) {
-    await BrowserHelper.page.locator(elLocator).click();
+  static async getCount(selector: string) {
+    try {
+      await BrowserHelper.page.waitForSelector(selector);
+      return await BrowserHelper.page.$$eval(selector, element => element.length);
+    } catch (error) {
+      throw new Error(`Can't get count for the selecor: ${selector}`);
+    }
   }
 
-  static async setValue(locator: string, value: string) {
-    await BrowserHelper.page.click(locator, { clickCount: 3 });
+  static async clickOnElement(selector: string) {
+    try {
+      await BrowserHelper.page.waitForSelector(selector);
+      await BrowserHelper.page.click(selector);
+    } catch (error) {
+      throw new Error(`Could not click on selector: ${selector}`);
+    }
+  }
+
+  static async setValue(selector: string, value: string) {
+    await BrowserHelper.page.click(selector, { clickCount: 3 });
     await BrowserHelper.page.keyboard.press('Backspace');
-    await BrowserHelper.page.type(locator, value);
+    await BrowserHelper.page.type(selector, value);
   }
 
-  public static async dragAndDropElement(locator: string, x: number, y: number) {
-    const preBoundingBox = await BrowserHelper.page.$(locator);
+  public static async dragAndDropElement(selector: string, x: number, y: number) {
+    const preBoundingBox = await BrowserHelper.page.$(selector);
     const boundingBox = await preBoundingBox.boundingBox();
     await BrowserHelper.page.mouse.move(boundingBox.x + boundingBox.width / 2, boundingBox.y + boundingBox.height / 2);
     await BrowserHelper.page.mouse.down();
     await BrowserHelper.page.mouse.move(x, y);
     await BrowserHelper.page.mouse.up();
   }
-  
-  public static async getText(locator: string, isInnerText = false) {
-    const element = await BrowserHelper.page.$(locator);
-    if (isInnerText) {
-        return element?.getProperty('innerHTML')
-    } else {
-        return element?.contentFrame()
+
+  public static async getText(selector: string) {
+    try {
+      await BrowserHelper.page.waitForSelector(selector);
+      return await BrowserHelper.page.$eval(selector, element => element.innerHTML);
+    } catch (error) {
+      throw new Error(`Cannot get text from the selector: ${selector}`);
     }
-}
+  }
   public async isElementDisplayed(element) {
     const isVisibleHandle = await BrowserHelper.page.evaluateHandle(el => {
       const style = window.getComputedStyle(el);
